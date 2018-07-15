e7bec7fc321d8ef9167482db858128ad7db2b758
mjcampagna <mjcampagna@gmail.com>
2018-07-15 14:43:30 -0700

    Refactor game logic out of component, into separate file

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -6,34 +6,23 @@ import Game from './gameLogic.js';
 export default class Board extends React.Component {
 	constructor(props) {
 		super(props);
+		this.game = null;
 		this.state = {
 			board: null,
 			bombs: 10,
-			gridSize: 10,
-			totalRevealed: 0
+			gridSize: 10
 		}
 	}
 
 	componentDidMount() {
-		const board = this.generateNewBoard( this.state.gridSize );
+		this.game = new Game(
+			this.state.gridSize,
+			this.state.bombs
+		);
+		const board = this.game.generateNewBoard( this.state.gridSize );
 		this.setState({
 			board: board
 		});
-	}
-
-	randomizeCoordinate(gridSize) {
-		return Math.floor( Math.random() * gridSize );
-	}
-
-	nextTurn(board, row, col) {
-		board = this.revealUninterestingNeighbors(board, row, col);
-		this.setState({
-			board: board
-		})
-	}
-
-	gameOver() {
-		console.log('BOOM!');
 	}
 
 	handleClickOnCell(event) {
@@ -41,105 +30,11 @@ export default class Board extends React.Component {
 		const target = event.target;
 		const row = parseInt(target.getAttribute('data-row'), 10);
 		const col = parseInt(target.getAttribute('data-col'), 10);
-		if ( !board[row][col].revealed ) {
-			if ( event.ctrlKey || event.metaKey ) {
-				board[row][col].flag = !board[row][col].flag;
-				this.setState({
-					board: board
-				});
-				return;
-			}
-	
-			if ( !board[row][col].flag ) {
-				if ( board[row][col].bomb ) {
-					if ( this.state.totalRevealed === 0 ) {
-						board = this.generateNewBoard( this.state.gridSize );
-						this.setState({
-							board: board
-						}, () => {
-							this.nextTurn(board, row, col);
-							return;
-						});
-					} else {
-						this.gameOver();
-					}
-				}
-	
-				this.nextTurn(board, row, col);
-			}
-		}
-	}
 
-	revealUninterestingNeighbors(board, row, col) {
-		if ( !board[row][col].revealed ) {
-			board[row][col].revealed = true;
-			this.setState({
-				totalRevealed: this.state.totalRevealed + 1
-			})
-			for ( let r = row-1; r <= row+1; r++ ) {
-				if ( board[r] ){
-					for ( let c = col-1; c <= col+1; c++ ) {
-						if ( board[r][c] ) {
-							if ( board[row][col].near === 0 ) {
-								board = this.revealUninterestingNeighbors(board, r, c);
-							}
-						}
-					}
-				}
-			}	
-		}
-		return board;
-	}
-
-	distributeBombs(board, bombs) {
-		for ( var i = 0; i < bombs; i++ ) {
-			const row = this.randomizeCoordinate(this.state.gridSize);
-			const col = this.randomizeCoordinate(this.state.gridSize);
-			if ( board[row][col].bomb === false ) {
-				board[row][col].bomb = true;
-			} else {
-				i--;
-			}
-		}
-		return board;
-	}
-
-	findNeighboringBombs(board) {
-		for ( let row = 0; row < this.state.gridSize; row++ ) {
-			for ( let col = 0; col < this.state.gridSize; col++ ) {
-				let near = board[row][col].near;
-				near = board[row-1] && board[row-1][col] !== undefined && board[row-1][col].bomb ? near += 1 : near;
-				near = board[row-1] && board[row-1][col+1] && board[row-1][col+1].bomb ? near += 1 : near;
-				near = board[row][col+1] && board[row][col+1].bomb ? near += 1 : near;
-				near = board[row+1] && board[row+1][col+1] && board[row+1][col+1].bomb ? near += 1 : near;
-				near = board[row+1] && board[row+1][col] && board[row+1][col].bomb ? near += 1 : near;
-				near = board[row+1] && board[row+1][col-1] && board[row+1][col-1].bomb ? near += 1 : near;
-				near = board[row][col-1] && board[row][col-1].bomb ? near += 1 : near;
-				near = board[row-1] && board[row-1][col-1] && board[row-1][col-1].bomb ? near += 1 : near;
-				board[row][col].near = near;
-			}
-		}
-		return board;
-	}
-
-	generateNewBoard(size) {
-		let board = [];
-		for ( let y = 0; y < size; y++ ) {
-			let newArray = new Array(size);
-			for ( let i = 0; i < newArray.length; i++ ) {
-				newArray[i] = {
-					bomb: false,
-					flag: false,
-					near: 0,
-					revealed: false
-				};
-			}
-			board.push(newArray);
-		}
-
-		board = this.distributeBombs(board, this.state.bombs);
-		board = this.findNeighboringBombs(board);
-		return board;
+		board = this.game.playerAction(event, board, row, col);
+		this.setState({
+			board: board
+		});
 	}
 
 	renderLoading() {
---
+++ src/components/gameLogic.js
@@ -0,0 +1,118 @@
+export default class Game {
+	constructor(gridSize, bombs) {
+		this.gridSize = gridSize;
+		this.bombs = bombs;
+		this.totalRevealed = 0;
+	}
+
+	generateNewBoard(gridSize) {
+		let board = [];
+		for ( let y = 0; y < gridSize; y++ ) {
+			let newArray = new Array(gridSize);
+			for ( let i = 0; i < newArray.length; i++ ) {
+				newArray[i] = {
+					bomb: false,
+					flag: false,
+					near: 0,
+					revealed: false
+				};
+			}
+			board.push(newArray);
+		}
+		board = this.distributeBombs(board, this.bombs);
+		board = this.findNeighboringBombs(board);
+		return board;
+	}
+
+	randomizeCoordinate(gridSize) {
+		return Math.floor( Math.random() * gridSize );
+	}
+
+	distributeBombs(board, bombs) {
+		for ( var i = 0; i < bombs; i++ ) {
+			const row = this.randomizeCoordinate(this.gridSize);
+			const col = this.randomizeCoordinate(this.gridSize);
+			if ( board[row][col].bomb === false ) {
+				board[row][col].bomb = true;
+			} else {
+				i--;
+			}
+		}
+		return board;
+	}
+
+	findNeighboringBombs(board) {
+		for ( let row = 0; row < this.gridSize; row++ ) {
+			for ( let col = 0; col < this.gridSize; col++ ) {
+				let near = board[row][col].near;
+
+				for ( let r = row-1; r <= row+1; r++ ) {
+					if ( board[r] ){
+						for ( let c = col-1; c <= col+1; c++ ) {
+							if ( board[r][c] ) {
+								near = board[r][c].bomb ? near += 1 : near;
+							}
+						}
+					}
+				}
+
+				board[row][col].near = near;
+			}
+		}
+		return board;
+	}
+
+	revealUninterestingNeighbors(board, row, col) {
+		if ( !board[row][col].revealed ) {
+			board[row][col].revealed = true;
+			this.totalRevealed += 1;
+
+			for ( let r = row-1; r <= row+1; r++ ) {
+				if ( board[r] ){
+					for ( let c = col-1; c <= col+1; c++ ) {
+						if ( board[r][c] ) {
+							if ( board[row][col].near === 0 ) {
+								board = this.revealUninterestingNeighbors(board, r, c);
+							}
+						}
+					}
+				}
+			}
+
+		}
+		return board;
+	}
+
+	playerAction(event, board, row, col) {
+		if ( !board[row][col].revealed ) {
+
+			// toggle flag
+			if ( event.ctrlKey || event.metaKey ) {
+				board[row][col].flag = !board[row][col].flag;
+				return board;
+			}
+			if ( !board[row][col].flag ) {
+
+				// is bomb?
+				if ( board[row][col].bomb ) {
+					if ( this.totalRevealed === 0 ) {
+						board = this.generateNewBoard( this.gridSize );
+					} else {
+						this.gameOver();
+						return board;
+					}
+				}
+
+				// reveal tile(s)
+				board = this.revealUninterestingNeighbors(board, row, col);
+
+			}
+		}
+		return board;
+	}
+
+	gameOver() {
+		console.log('BOOM!');
+	}
+
+} // Game 
